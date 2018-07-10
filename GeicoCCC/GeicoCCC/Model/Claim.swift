07c8acaa800f9fc0589e9c5bb86beaf6a9238686
Flaviu Suciu <flaviu.suciu@3pillarglobal.com>
2018-07-10 13:29:48 +0300

    Claims Screen & Login Functionality

---
+++ GeicoCCC/GeicoCCC/Model/Claim.swift
@@ -0,0 +1,14 @@
+//
+//  Claim.swift
+//  GeicoCCC
+//
+//  Created by Flaviu Adrian Suciu on 06/07/2018.
+//  Copyright © 2018 Geico. All rights reserved.
+//
+
+import UIKit
+
+struct Claim {
+    let claimID: String
+    let userName: String
+}
